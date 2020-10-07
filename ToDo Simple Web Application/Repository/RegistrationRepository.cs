01e62570c83e40def8ed0c49f724fa39a705f517
Sundaram Subramanian <learnwithsundar@gmail.com>
2020-10-08 04:39:43 +0530

    Added Registration Service
    Added Registration Service

--- ToDo Simple Web Application/Repository/RegistrationRepository.cs
+++ ToDo Simple Web Application/Repository/RegistrationRepository.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ToDo_Simple_Web_Application.Interface;
 
 namespace ToDo_Simple_Web_Application.Repository
 {
-    public class RegistrationRepository
+    public class RegistrationRepository : IRegistrationRepository
     {
     }
 }
