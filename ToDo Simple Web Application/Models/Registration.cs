3177d82445e1c2678ec3a980bc6b36fba78f8923
Sundaram Subramanian <learnwithsundar@gmail.com>
2020-10-07 14:25:08 +0530

    Added Models
    Added Models

---
+++ ToDo Simple Web Application/Models/Registration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDo_Simple_Web_Application.Models
+{
+    public class Registration
+    {
+    }
+}
